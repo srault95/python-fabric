fd558f17dc123c3d59a1a2987c5546f6821f9862
LBN <lbn@lbn.fr>
2018-03-11 21:27:46 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Python Fabric3 portable
+
+**Compilation de Python 2.7 & Fabric3 dans un binaire portable avec Pyinstaller**
+
---
+++ build-fabric.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+DIST_PATH=/app/dist
+SOURCE_MAIN=/usr/local/python2.7/bin/fab
+TARGET_NAME=fab
+
+pip install fabric3
+
+pyinstaller --clean --distpath=${DIST_PATH} -y -F ${SOURCE_MAIN} --name=${TARGET_NAME}
+
